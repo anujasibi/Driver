b39128c97fe7bab487463dfd958e0401eba5abf6
pm <pm@creopedia.com>
2019-09-03 12:52:46 +0530

    CHANGES

--- app/src/main/java/creo/com/driver/Profile.java
+++ app/src/main/java/creo/com/driver/Profile.java
@@ -6,16 +6,24 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 import java.util.ArrayList;
 
 public class Profile extends AppCompatActivity {
     private RecyclerView recyclerView;
     Context mContext=this;
+    private RelativeLayout rl,rlt;
+    ScrollView scrollView;
+
 
     @SuppressLint("WrongConstant")
     @Override
@@ -25,12 +33,36 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         final ArrayList<NotificationPojo> pojo = new ArrayList<>();
-        NotificationPojo notificationPojo = new NotificationPojo("You have recieved 5 new compliments",R.drawable.notification);
+        NotificationPojo notificationPojo = new NotificationPojo("You have recieved 5 new compliments",R.drawable.star);
         NotificationPojo notificationPojo1=new NotificationPojo("You have recieved a new policy update",R.drawable.notification);
         pojo.add(notificationPojo);
         pojo.add(notificationPojo1);
 
         recyclerView = findViewById(R.id.rer);
+        scrollView=findViewById(R.id.scroll);
+        scrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                scrollView.fullScroll(scrollView.FOCUS_UP);
+            }
+        });
+        rl=findViewById(R.id.rl);
+        rlt=findViewById(R.id.rlt);
+        rlt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, Review.class));
+                Animatoo.animateSlideDown(Profile.this);
+
+            }
+        });
+        rl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Profile.this, Earning.class));
+                Animatoo.animateSlideUp(Profile.this);
+            }
+        });
         recyclerView.setNestedScrollingEnabled(false);
         NotificationAdapter notificationAdapter = new NotificationAdapter(pojo,mContext);
 

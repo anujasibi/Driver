3ae7276f479f5c1622962469e17d3380fd9438e7
pm <pm@creopedia.com>
2019-09-03 17:25:37 +0530

    Changes

--- app/src/main/java/creo/com/driver/Earning.java
+++ app/src/main/java/creo/com/driver/Earning.java
@@ -2,15 +2,20 @@ package creo.com.driver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class Earning extends AppCompatActivity {
 
     LinearLayout linearChart;
+    TextView withdraw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,14 @@ public class Earning extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_earning);
         linearChart = (LinearLayout) findViewById(R.id.linearChart);
+        withdraw=findViewById(R.id.wao);
+        withdraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Earning.this, PaymentList.class));
+                Animatoo.animateZoom(Earning.this);
+            }
+        });
         int colerloop[] = { 1, 2, 1, 1,1,2,3 };
         int heightLoop[] = { 300, 100, 400, 200,300 ,300,500};
         for (int j = 0; j < colerloop.length; j++) {
--- app/src/main/java/creo/com/driver/MainUI.java
+++ app/src/main/java/creo/com/driver/MainUI.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
@@ -15,6 +16,7 @@ public class MainUI extends AppCompatActivity {
 
     ImageView im;
     Context context;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +26,22 @@ public class MainUI extends AppCompatActivity {
         setContentView(R.layout.activity_main_ui);
 
         im=findViewById(R.id.dr);
+        textView=findViewById(R.id.text);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainUI.this, CustomerDetails.class));
+                Animatoo.animateSlideUp(MainUI.this);
+            }
+        });
 
         im.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 startActivity(new Intent(MainUI.this, Profile.class));
-                Animatoo.animateInAndOut(MainUI.this);
+                Animatoo.animateDiagonal(MainUI.this);
             }
         });
     }
